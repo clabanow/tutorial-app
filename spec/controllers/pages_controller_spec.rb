1ca23c14ae718b9fdce415b67a6971aa4c7ef4fe
Charles A. Labanowski <clabanow@gmail.com>
2014-09-16 16:28:29 +0200

    Adds basic layout file stubs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+  let(:app_name) { 'Tutorial App' }
+  subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_title(get_full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:page_title) { "" }
+
+    it_should_behave_like "all static pages"
+  end
 end
